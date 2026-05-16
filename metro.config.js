b568d4134ecc5d4ae53b43e8a8dd31777de4dce3
AnjahNyOny <110890442+AnjahNyOny@users.noreply.github.com>
2026-05-16 18:57:51 -0400

    ⚙️ Configuration de metro.config.js pour le support SVG
    - Ajout de react-native-svg-transformer dans le resolver/transformer de Metro
    - Permet d'importer directement les fichiers .svg comme composants React Native sans crasher l'application (Element type is invalid)

--- metro.config.js
+++ metro.config.js
@@ -8,4 +8,16 @@ config.resolver.blockList = [
   new RegExp(path.resolve(__dirname, 'relay-server') + '/.*'),
 ];
 
+// Configure SVG transformer
+const { transformer, resolver } = config;
+config.transformer = {
+  ...transformer,
+  babelTransformerPath: require.resolve("react-native-svg-transformer")
+};
+config.resolver = {
+  ...resolver,
+  assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+  sourceExts: [...resolver.sourceExts, "svg"]
+};
+
 module.exports = config;
